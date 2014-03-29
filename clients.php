d90eb81335630cc042ade3d3d9623dd6277ceecc
linepogl <linepogl@gmail.com>
2014-03-29 19:57:03 +0100

    clients

--- clients.php
+++ clients.php
@@ -3,6 +3,19 @@ include('_.php');
 LT::Head();
 ?>
 
+<div class="clients">
+<?php
+$a = array();
+foreach (scandir('cli') as $f) {
+    if ($f == '.' || $f == '..') continue;
+    $a[] = $f;
+}
+shuffle($a);
+foreach ($a as $f) {
+    echo '<img class="client" src="cli/'.$f.'" />';
+}
+?>
+</div>
 
 
 <?php
