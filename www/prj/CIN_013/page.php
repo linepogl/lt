6d3321d287dd5c54051513a08cb33fa1618b44fb
Lazare Inepologlou <linepogl@gmail.com>
2024-03-23 11:19:14 +0100

    update 2024

--- www/prj/CIN_013/page.php
+++ www/prj/CIN_013/page.php
@@ -1,21 +1,5 @@
-<iframe class="vimeo" src="//player.vimeo.com/video/123516589" width="100%" height="350" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-<iframe class="vimeo" src="//player.vimeo.com/video/123516590" width="100%" height="350" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+<iframe class="vimeo" src="//player.vimeo.com/video/93200904" width="100%" height="350" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 
-
-<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236546681&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
-<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236545424&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
-<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236543257&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
-
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo1.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo2.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo3.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo4.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo5.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo6.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo7.jpg" /></div>
+<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/80147813&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
+<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/80147021&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
+<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/80145657&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
