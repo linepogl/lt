2ece2f01d47e222f7d62fdf1e944f0c95e212ca7
Lazare Inepologlou <linepogl@gmail.com>
2024-03-23 11:13:16 +0100

    update 2024

--- www/prj/CIN_015/page.php
+++ www/prj/CIN_015/page.php
@@ -1,14 +1,8 @@
 
-
-<iframe class="soundcloud" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/636611904&color=%232c2c34&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+<iframe class="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/523890027&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false"></iframe>
 
 <div><img class="photo" src="<?= $this->GetPath() ?>/photo1.jpg" /></div>
 <br/><br/>
 <div><img class="photo" src="<?= $this->GetPath() ?>/photo2.jpg" /></div>
 <br/><br/>
 <div><img class="photo" src="<?= $this->GetPath() ?>/photo3.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo4.jpg" /></div>
-<br/><br/>
-<div><img class="photo" src="<?= $this->GetPath() ?>/photo5.jpg" /></div>
-
